9ebf21de7a34996dfdfca27c2b44ff658dffc0f5
Lenovo - Yoga\Daniel <dlund13@student.aau.dk>
2019-03-21 08:24:32 +0100

    Update Preprocess.py
    Funktionel flattening.

--- Preprocess.py
+++ Preprocess.py
@@ -22,7 +22,7 @@ import matplotlib.patches as mpatches
 
 # NORMAL, DRUSEN, DME eller CNV NORMAL-1001666-1 #NORMAL-1001772-1
 
-path = 'C:\\Users\\danie\\Desktop\\ST8\\Projekt\\Data\\'
+path = 'C:\\Users\\danie\\Desktop\\Misc\\ST7\\OCT---Transferlearning\\'
 filename = os.path.join(path, 'DRUSEN-1001666-1.JPEG')
 #filename = 'C:\\Users\\danie\\Desktop\\ST8\\Projekt\\Data\\NORMAL-1001666-1.jpg'
 test_im = io.imread(filename)
@@ -46,10 +46,18 @@ plt.figure;plt.imshow(dog, 'gray', interpolation = 'none');plt.imshow(mp.skeleto
 # Use the value as weights later
 weights = test_im[dog] / float(test_im.max())
 
-
+# Recasts DoG to int
 dog = dog*1
-dog_label = ms.label(dog)
 
+# Label regioner
+dog_label = ms.label(dog)
+'''
+For hver region, find start raekke og soejle og vurdér om regionen er (1) er større
+end 2000 pixels, (2) om den har pixel vaerdier indenfor de foerste 100 raekker
+og (3) om der er pixel vaerdier indenfor de sidste 100 raekker. 
+Er et af disse opfyldt, så slet regionen i det filtrerede billede
+
+'''
 for pred_region in ms.regionprops(dog_label):
     minr, minc, maxr, maxc = pred_region.bbox
     if pred_region.area < 2000 or minr < 100 or maxr > len(dog_label)-100:
@@ -62,12 +70,6 @@ plt.subplot(1,2,2);plt.imshow(dog);
 
 # Get coordinates of pixels corresponding to marked region
 X = np.argwhere(dog)
-
-# Use the value as weights later
-#weights = test_im[dog] / float(test_im.max())
-
-# Convert to diagonal matrix
-W = np.diag(weights)
 
 # Column indices
 x = X[:, 1].reshape(-1, 1)
@@ -85,25 +87,19 @@ order = 2
 A = ppu.feature(x, order)
 # w = inv (A^T A + alpha * I) A^T y
 w_unweighted = np.linalg.pinv( A.T.dot(A) + alpha * np.eye(A.shape[1])).dot(A.T).dot(y)
-# w = inv (A^T W A + alpha * I) A^T W y
-#w_weighted = np.linalg.pinv( A.T.dot(W).dot(A) + alpha * \
-                             #np.eye(A.shape[1])).dot(A.T).dot(W).dot(y)
 
 
 # Generate test points
-n_samples = 50
+n_samples = test_im.shape[1]
 x_test = np.linspace(0, test_im.shape[1], n_samples)
 X_test = feature(x_test, order)
 
 # Predict y coordinates at test points
 y_test_unweighted = X_test.dot(w_unweighted)
-#y_test_weighted = X_test.dot(w_weighted)
 
 
 
-
-##!!!!!!!!! ALTERNATIVT PRØV np.nonzero til at få X og Y koordinater til brug i 
-##!!!!!!!!! nedenstående funktion
+##!!!!!!!!! ALTERNATIV Metode - RANSAC !!!!!!!!!!!!!!##
 dog_zeros = np.nonzero(dog)
 
 
@@ -113,18 +109,36 @@ poly = np.poly1d(bestfit)
 x = np.linspace(0, test_im.shape[1], n_samples)
 y = poly(x)
 
-#plt.plot(dog_zeros[1], y)
-
 # Display
 fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 ax.imshow(test_im)
 ax.plot(x_test, y_test_unweighted, color="green", marker='o', label="Unweighted")
 ax.plot(x, y, color="blue", marker='o', label="RANSAC")
-#ax.plot(x_test, y_test_weighted, color="blue", marker='x', label="Weighted")
 fig.legend()
 #fig.savefig("curve.png")
+#'''
+plt.close()
+y_diff = np.diff(np.round(y))
+#y_diffR = np.round(y_diff)
 
-### Test med Shepard distortion og måske noget forankring? ###
+plt.figure;plt.subplot(1,2,1);plt.imshow(test_im);
+test_new = test_im
+n_roll = []
+for i in range(test_im.shape[1]-1):
+    temp = int(np.round(y[i])-np.round(y[i+1]))
+    n_roll.append(temp)
+    test_new[:,i+1] = np.roll(test_new[:,i+1], temp)
+    y[i+1] = y[i+1] + temp
+
+plt.subplot(1,2,2);plt.imshow(test_new)
+#plt.figure;plt.imshow(test_new)
 
 
+'''    
+Roll with a negative shift:
 
+x2[:, 0] = np.roll(x2[:, 0], -2)
+Roll with a positive shift:
+
+x2[:, 4] = np.roll(x2[:, 4], 2)
+'''
